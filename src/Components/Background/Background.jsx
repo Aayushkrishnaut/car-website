1b03c43b9312f5ad8de1422cc3b4ada84549d1ac
Aayushkumar Yadav <140250471+Aayushkrishnaut@users.noreply.github.com>
2024-10-04 13:51:00 +0530

    Update Background.jsx

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -9,16 +9,16 @@ import image3 from '../../assets/image3.png';
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className='background fade-in' autoPlay loop muted>
+      <video className='background ' autoPlay loop muted>
         <source src={video1} type='video/mp4' />
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className='background fade-in' alt='' />;
+    return <img src={image1} className='background ' alt='' />;
   } else if (heroCount === 1) {
-    return <img src={image2} className='background fade-in' alt='' />;
+    return <img src={image2} className='background ' alt='' />;
   } else if (heroCount === 2) {
-    return <img src={image3} className='background fade-in' alt='' />;
+    return <img src={image3} className='background ' alt='' />;
   }
   return null; // Add a default return
 };
